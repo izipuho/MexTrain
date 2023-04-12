3b4b1fa2fe07bd50b1e3bbd88295e4ae447c4333
izipuho <58221294+izipuho@users.noreply.github.com>
2023-04-12 22:47:38 +0300

    Dealed with table class. Game round:  easy is ok  normal brakes on break. need to fix.

--- mextrain.py
+++ mextrain.py
@@ -1,13 +1,14 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.9
 import random
-import json
 
-#in_max_tile = int(input('What is the maximum tile: '))
-#in_players_count = int(input('How much players are we awaiting: '))
-#in_difficulty = input('What is the difficulty: ')
+# in_max_tile = int(input('What is the maximum tile: '))
+# in_players_count = int(input('How much players are we awaiting: '))
+# in_difficulty = input('What is the difficulty: ')
 in_max_tile = 12
 in_players_count = 4
 in_difficulty = 'normal'
+
+
 # define tile
 class tile:
     def __init__(self, numbers):
@@ -22,32 +23,35 @@ class tile:
     def is_double(self):
         if self.numbers[0] == self.numbers[1]:
             print(f'{self.numbers} is double.')
-            return(True)
+            return True
         else:
             print(f'{self.numbers} not a double.')
-            return(False)
+            return False
 
     def flip(self):
-        print(f'Tile {self.text} will be fliped to {self.text_flipped}')
+        # print(f'Tile {self.text} will be fliped to {self.text_flipped}')
         self.numbers.reverse()
 
     def is_suitable(self, number):
         if number in self.numbers:
+            # print(f'{self.numbers} vs {number}: ok!')
             if number == self.numbers[1]:
                 self.flip()
-            return(True)
+            return True
         else:
-            return(False)
-
-    def __str__(self):
-        return(f'[{self.numbers[0]:>2}|{self.numbers[1]:>2}]')
+            # print(f'{self.numbers} vs {number}: No!')
+            return False
 
     def __repr__(self):
-        return(f'Tile {self.numbers} scores {self.score} points.')
+        return f'[{self.numbers[0]:>2}|{self.numbers[1]:>2}]'
+
+    def __str__(self):
+        return f'Tile {self.numbers} scores {self.score} points.'
+
 
 class tile_set:
     def __init__(self):
-        self.max_tile = in_max_tile#[in_max_tile, in_max_tile]
+        self.max_tile = in_max_tile  # [in_max_tile, in_max_tile]
         self.set = dict()
         self.set_repr = str()
         self.create()
@@ -56,55 +60,54 @@ class tile_set:
     def create(self):
         for t in range(in_max_tile, -1, -1):
             for i in range(t, -1, -1):
-                tl = tile([t,i])
-                #print(tl)
+                tl = tile([t, i])
+                # print(tl)
                 self.set[tl.code] = tl
                 self.set_repr += str(tl) + ' '
             self.set_repr += '\n'
-        #random.shuffle(self.set)
 
     def __str__(self):
-        return(f'Got set of {len(self.set)} tiles with {self.max_tile} highest.')
+        return f'Got set of {len(self.set)} tiles with {self.max_tile} highest.'
 
     def __repr__(self):
-        return(self.set_repr)
+        return self.set_repr
 
-#TODO messed up with tile set
+
 class table:
     def __init__(self, players):
-        self.players = players 
+        self.players = players
         self.tile_set = tile_set()
         self.max_tile = self.tile_set.max_tile
-        #self.table_tile_cnt = len(self.tile_set.set)
+        # self.table_tile_cnt = len(self.tile_set.set)
         # Set hand tile_count. Not by the rules, but why not to make it?
-        self.hand_tile_cnt = self.tile_set.max_tile#in_max_tile#12
+        self.hand_tile_cnt = self.tile_set.max_tile  # in_max_tile#12
         if self.players == 2:
             self.hand_tile_cnt += 3
         # Init scores
         self.scores = dict()
-        for p in range(1, self.players+1):
+        for p in range(1, self.players + 1):
             self.scores[p] = 0
         self.scores['Table'] = 0
         self.layout = dict()
 
     def deal(self, round):
+        print('Dealing...')
         tile_set = self.tile_set.set.copy()
         # Remove start tile. Ugly: not by tile class. MayBe TODO
         del tile_set[f'{round}-{round}']
-        ##tile_set.remove(tile([round, round]))
-        # init hands
+        # Init hands
         hands = dict()
-        trails = dict() 
-        for p in range(1, self.players+1):
+        trails = dict()
+        for p in range(1, self.players + 1):
             hands[p] = []
             trails[p] = ['Closed', dict()]
         trails['Table'] = ['Opened', [[round, round]]]
         # deal
         for t in range(0, self.hand_tile_cnt):
-            for p in range(1, self.players+1):
+            for p in range(1, self.players + 1):
                 # get random tile
                 ##n = random.randint(0, len(tile_set)-1)
-                #print("deal: {order}'th tile out of {total}".format(order = n, total = len(tile_set)))
+                # print("deal: {order}'th tile out of {total}".format(order = n, total = len(tile_set)))
                 ##hands[p].append(tile_set[n])
                 tl = random.choice(list(tile_set.values()))
                 hands[p].append(tl)
@@ -112,21 +115,34 @@ class table:
                 del tile_set[tl.code]
         hands['Table'] = tile_set
         table_tile_cnt = len(hands['Table'])
-        print(f'Dealt {self.hand_tile_cnt} tiles for {self.players} players. Got {table_tile_cnt} tiles left on table and {[round, round]} is strating tile.\n')
-        self.layout = {'hands': hands, 'trails': trails, 'moves': 0}
+        #print(f'Dealt {self.hand_tile_cnt} tiles for {self.players} players. Got {table_tile_cnt} tiles left on table and {[round, round]} is strating tile.\n')
+        self.layout = {'hands': hands, 'trails': trails, 'moves': 0, 'round': [round, 'Dealt']}
 
     def __str__(self):
-        #return(f'Got set of {self.table_tile_cnt} tiles.')
-        pass
+        table_str = f'We have {self.players} players at the table'
+        if len(self.layout) == 0:
+            table_str += '. No tiles are dealt. Game not started.'
+        else:
+            table_str += f"... still. All tiles are dealt. {len(self.layout['hands']['Table'])} tiles left. Current round is {self.layout['round'][0]}."
+        return table_str
+
+    def __repr__(self):
+        layout_repr = str()
+        if self.layout['round'][1] == 'Dealt':
+            layout_repr = 'Trails are:\n'
+            for p in range(1, self.players + 1):
+                layout_repr += f"Player {p} has trail:\n"
+                layout_repr += f"\t{self.layout['trails'][p]}"
+            return layout_repr
+        else:
+            pass
 
 class game_round:
     def __init__(self, table, round_num):
         self.num = round_num
         self.table = table
         self.table.deal(self.num)
-        #print(self.table)
-        print(f'Round {self.num}:')
-        self.hands = self.table.table['hands']
+        self.hands = self.table.layout['hands']
 
     # Let the magic happen
     def init_trail(self, player, difficulty):
@@ -135,84 +151,73 @@ class game_round:
         trail = []
         init_number = self.num
         print(f'\tPlayer {player} had hand: {hand}')
-        if difficulty == 'easy':
+        if difficulty in ('easy', 'e', '0'):
             # order based
             while len(hand) != 0 and init_number != -1:
-                #print(f'Looking for {init_number}')
-                #print(f'Hand tile count: {len(hand)}')
+                # print(f'Looking for {init_number}')
+                # print(f'Hand tile count: {len(hand)}')
                 for t in hand:
-                    #print(f'{hand.index(t) + 1}. Current tile: {t}')
-                    if hand.index(t)+1 == len(hand):
-                        #print('None found')
+                    # print(f'{hand.index(t) + 1}. Current tile: {t}')
+                    if t.is_suitable(init_number):
+                        # print(f'{t} is ok')
+                        hand.remove(t)
+                        trail.append(t)
+                        init_number = t.numbers[1]
+                        break
+                    if hand.index(t) + 1 == len(hand):
+                        # print('None found')
                         init_number = -1
                         break
-                    if init_number in t:
-                        #print(f'{t} is ok')
-                        t_aligned = t
-                        hand.remove(t)
-                        if init_number == t[0]:
-                            init_number = t[1]
-                        else:
-                            init_number = t[0]
-                            t_aligned = [t[1], t[0]]
-                        trail.append(t_aligned)
-                        break
-        elif difficulty == 'normal':
+        # TODO messed up with tile_set and tile classes
+        elif difficulty in ('normal', 'n', '1'):
             # max based without doubles
             i = 0
             while len(hand) != 0 and init_number != -1 and i < 100:
-                #print(f'Hand tile count: {len(hand)}')
-                #print(f'Looking for {init_number}')
+                print(f'Hand tile count: {len(hand)}')
+                print(f'Looking for {init_number}')
                 second_number = -1
                 for t in hand:
-                    #print(f'{hand.index(t) + 1}. Current tile: {t}. Current max number: {second_number}. Looking for init {init_number}.')
-                    if init_number in t:
-                    #if init_number == t[0] or init_number == t[1]:
-                        #print(f'{t} is ok')
-                        t_aligned = t
-                        if init_number != t[0]:
-                            #print('Tile reversed')
-                            t_aligned = [t[1], t[0]]
-                        if second_number < t_aligned[1]:
-                            #print('Changed tile')
-                            second_number = t_aligned[1]
-                        elif second_number == t_aligned[1]:
+                    print(f'{hand.index(t) + 1}. Current tile: {t}. Current max number: {second_number}. Looking for init {init_number}.')
+                    if t.is_suitable(init_number):
+                        print(f'{t} is ok')
+                        #TODO something bad with this breaking loop
+                        if t.is_double:
                             print('Double is always good. Take it.')
-                            second_number = t_aligned[1]
+                            good_tile = t
                             break
+                        elif second_number < t.numbers[1]:
+                            print('More is better. Changed tile')
+                            good_tile = t
+                            second_number = t.numbers[1]
                         else:
-                            #print('Old one is good')
+                            print('Old one is good')
                             pass
-                        t_aligned = [init_number, second_number]
                 init_number = second_number
                 if second_number != -1:
-                    if t_aligned not in hand:
-                        hand.remove([t_aligned[1], t_aligned[0]])
-                    else:
-                        hand.remove(t_aligned)
-                    trail.append(t_aligned)
-                    #print(f'We take {t_aligned} to trail.\n')
+                    hand.remove(good_tile)
+                    trail.append(good_tile)
+                    print(f'We take {repr(good_tile)} to trail.\n')
                 else:
                     break
                 i += 1
-        self.table.table['trails'][player][1] = trail
+        self.table.layout['trails'][player][1] = trail
         if len(trail) == 0:
-            self.table.table['trails'][player][0] = 'Empty'
+            self.table.layout['trails'][player][0] = 'Empty'
             print(f'\t\tPlayer {player} has no trail')
         else:
             print(f'\t\tPlayer {player} has init trail {len(trail)} tiles long: {trail}')
+        self.table.layout['round'][1] = 'Init trails'
         print('\n')
 
     def draw(self, player):
         # get random tile
-        n = random.randint(0, len(self.table.table['hands']['Table'])-1)
-        #print("deal: {order}'th tile out of {total}".format(order = n, total = len(tile_set)))
+        n = random.randint(0, len(self.table.table['hands']['Table']) - 1)
+        # print("deal: {order}'th tile out of {total}".format(order = n, total = len(tile_set)))
         self.table.table['hands'][player].append(self.table.table['hands']['Table'][n])
         print(f"Draw a tile from table: {self.table.table['hands']['Table'][n]}")
-        return(self.table.table['hands']['Table'][n])
+        return self.table.table['hands']['Table'][n]
         # delete it from set
         del self.table.table['hands']['Table'][n]
-
 
     def move(self, player):
         print(f"Move {self.table.table['moves']}. Player {player}.")
@@ -242,7 +247,7 @@ class game_round:
         print(f'Possible moves: {possible_moves}')
         if possible_moves['possible_cnt'] == 0:
             new_tile = self.draw(player)
-            #TODO
+            # TODO
         if self.table.table['trails'][player][0] != 'Empty':
             self.table.table['trails'][player][0] = 'Closed'
         self.table.table['moves'] += 1
@@ -252,7 +257,7 @@ class game_round:
         for k in self.hands:
             score = 0
             for t in self.hands[k]:
-                #print([t][0])
+                # print([t][0])
                 score += [t][0][0] + [t][0][1]
                 if t == [0, 0]:
                     score += 25
@@ -266,15 +271,16 @@ class game_round:
                     print(f'\tPlayer {k} has {len(self.hands[k])} tiles left in hand and scores {score}.')
                     print(f'\tHis tiles: {self.hands[k]}')
 
+
 class game:
     def __init__(self, players):
         self.players = players
         self.tbl = table(self.players)
-        self.first_player = randint(1, 4)
+        self.first_player = random.randint(1, 4)
         for rnd in range(self.tbl.max_tile, -1, -1):
-            #print(self.tbl)
+            # print(self.tbl)
             r = game_round(self.tbl, rnd)
-            for p in range(1, self.players+1):
+            for p in range(1, self.players + 1):
                 r.init_trail(p, in_difficulty)
             r.calc_hands()
 
@@ -287,6 +293,7 @@ class game:
             i += 1
 
 tbl = table(in_players_count)
-print(repr(tbl.tile_set))
-tbl.deal(1)
-print(tbl.layout)
+print(tbl)
+gr = game_round(tbl, 12)
+gr.init_trail(1, 'normal')
+print(tbl)
